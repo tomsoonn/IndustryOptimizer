58163ba88b54621b63df7b34a1f6b097540cb8b0
Tomsoonn <tomislawowo@wp.pl>
2018-08-09 11:14:08 +0200

    productionData_added_for_classification

--- src/main/java/agh/classification/ProductionData.java
+++ src/main/java/agh/classification/ProductionData.java
@@ -1,8 +1,129 @@
 package agh.classification;
 
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.functions.SMOreg;
+import weka.classifiers.functions.SimpleLinearRegression;
+import weka.classifiers.meta.MultiClassClassifier;
+import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ProductionData {
-    Instances trainData;
+
+    RandomForest tree = new RandomForest();
+    J48 tree2 = new J48();
+    SimpleLinearRegression reg = new SimpleLinearRegression();
+    MultilayerPerceptron mlp = new MultilayerPerceptron();
+
+    public ProductionData() {
+    }
+
+    public void trainAndTest() {
+        try {
+            DataSource source1 = new DataSource("train.arff");
+            DataSource source2 = new DataSource("test.arff");
+            Instances trainData = source1.getDataSet();
+            Instances testData = source2.getDataSet();
+            if (trainData.classIndex() == -1)
+                trainData.setClassIndex(trainData.numAttributes() - 1);
+            if (testData.classIndex() == -1)
+                testData.setClassIndex(testData.numAttributes() - 1);
+
+            //Standardize filter = new Standardize();
+            //filter.setInputFormat(trainData);
+            //trainData = Filter.useFilter(trainData, filter);
+            //testData = Filter.useFilter(testData, filter);
+
+            NumericToNominal toNominalFilter = new NumericToNominal();
+            toNominalFilter.setInputFormat(trainData);
+            String[] options = new String[2];
+            options[0] = "-R";                                    // "range"
+            options[1] = "2,4,6,8,10,12,14,16,18";
+            toNominalFilter.setOptions(options);     // set the options
+            trainData = Filter.useFilter(trainData, toNominalFilter);
+            testData = Filter.useFilter(testData, toNominalFilter);
+
+            //String[] options = new String[1];
+            //options[0] = "-U";            // unpruned tree
+            //tree.setOptions(options);     // set the options
+            tree.buildClassifier(trainData);
+
+            mlp.setLearningRate(0.3);
+            mlp.setMomentum(0.2);
+            mlp.setTrainingTime(2000);
+            mlp.setHiddenLayers("3");
+            //mlp.buildClassifier(trainData);
+
+            Evaluation eval = new Evaluation(trainData);
+
+            eval.evaluateModel(tree, testData);
+            System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void classify() {
+        Instances unlabeled = null;
+        try {
+            unlabeled = new Instances(new BufferedReader(new FileReader("unlab.arff")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // set class attribute
+        unlabeled.setClassIndex(unlabeled.numAttributes() - 1);
+
+
+        //Standardize filter = new Standardize();
+        //NumericToNominal filter1 = new NumericToNominal();
+        try {
+            //filter.setInputFormat(unlabeled);
+            //filter1.setInputFormat(unlabeled);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            //unlabeled = Filter.useFilter(unlabeled, filter);
+            //unlabeled = Filter.useFilter(unlabeled, filter1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Instances labeled = new Instances(unlabeled);
+
+        //System.out.println(labeled.toString());
+
+        // label instances
+        for (int i = 0; i < unlabeled.numInstances(); i++) {
+            double clsLabel = 0;
+            try {
+                clsLabel = tree.classifyInstance(unlabeled.instance(i));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            labeled.instance(i).setClassValue(clsLabel);
+            //System.out.println(clsLabel + " -> " + unlabeled.classAttribute().value((int) clsLabel));
+        }
+        System.out.println(unlabeled.toString() + "\nunalbeled");
+        System.out.println(labeled.toString() + "\ntree");
+
+        // save labeled data
+//        BufferedWriter writer = new BufferedWriter(
+//                new FileWriter("/some/where/labeled.arff"));
+//        writer.write(labeled.toString());
+//        writer.newLine();
+//        writer.flush();
+//        writer.close();
+    }
 
 }
--- src/main/java/agh/generator/Generator.java
+++ src/main/java/agh/generator/Generator.java
@@ -1,22 +1,15 @@
 package agh.generator;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
-import agh.Main;
 
 public class Generator {
     private PrintWriter writer;
 
-    public void generateRatio(int q,  Metals metal, boolean labeled) {
-        String filename = "w"+metal.toString();
-        try{
+    public void generateRatio(int q, Metals metal, boolean labeled) {
+        String filename = "w" + metal.toString() + ".arff";
+        try {
             writer = new PrintWriter(filename, "UTF-8");
             writer.print("@RELATION " + filename + "\n\n");
             writer.print("@ATTRIBUTE temperatura numeric\n");
@@ -29,34 +22,32 @@ public class Generator {
             writer.print("@ATTRIBUTE Magnez numeric\n");
             writer.print("@ATTRIBUTE Zelazo numeric\n");
             writer.print("@ATTRIBUTE Krzem numeric\n");
-            writer.print("@ATTRIBUTE w"+metal.toString()+" numeric\n");
+            writer.print("@ATTRIBUTE w" + metal.toString() + " numeric\n");
 
             writer.print("\n@DATA");
 
             System.out.println("generation started");
             Random r = new Random();
-            int temperature =0;
-            double ratio =0;
-            for (int j=0; j<q; j++){
+            int temperature;
+            double ratio;
+            for (int j = 0; j < q; j++) {
                 ArrayList<Integer> load = n_random(9);
                 writer.print("\n");
-                temperature = r.nextInt(1500)+500;
+                temperature = r.nextInt(1500) + 500;
                 writer.print(temperature);
                 writer.print(",");
-                for(int i=0; i<load.size(); i++){
+                for (int i = 0; i < load.size(); i++) {
                     writer.print(load.get(i));
                     writer.print(",");
                 }
-                if(temperature<getMinTemperature(metal)) {
+                if (temperature < getMinTemperature(metal)) {
                     ratio = 0;
-                }
-                else if (temperature<1000) {
+                } else if (temperature < 1000) {
                     ratio = getRatio(metal);
+                } else {
+                    ratio = getRatio(metal) + (temperature / 2000.0);
                 }
-                else {
-                    ratio = getRatio(metal)+(temperature/2000.0);
-                }
-                if(labeled) writer.print(ratio);
+                if (labeled) writer.print(ratio);
                 else writer.print("?");
 
             }
@@ -87,34 +78,54 @@ public class Generator {
         return load;
     }
 
-    public int getMinTemperature(Metals metal){
+    public int getMinTemperature(Metals metal) {
         switch (metal) {
-            case Aluminium : return 660;
-            case Miedz : return 1084;
-            case Nikiel : return 1453;
-            case Cynk : return 420;
-            case Olow : return 327;
-            case Cyna : return 232;
-            case Magnez : return 650;
-            case Zelazo : return 1500;
-            case Krzem : return 1410;
-            default: return -1;
+            case Aluminium:
+                return 660;
+            case Miedz:
+                return 1084;
+            case Nikiel:
+                return 1453;
+            case Cynk:
+                return 420;
+            case Olow:
+                return 327;
+            case Cyna:
+                return 232;
+            case Magnez:
+                return 650;
+            case Zelazo:
+                return 1500;
+            case Krzem:
+                return 1410;
+            default:
+                return -1;
         }
 
     }
 
-    public double getRatio(Metals metal){
+    public double getRatio(Metals metal) {
         switch (metal) {
-            case Aluminium : return 2;
-            case Miedz : return 1.5;
-            case Zelazo : return 1;
-            case Cynk : return 2.3;
-            case Olow : return 2.5;
-            case Cyna : return 2.8;
-            case Magnez : return 2;
-            case Krzem : return 1;
-            case Nikiel : return 1;
-            default: return -1;
+            case Aluminium:
+                return 2;
+            case Miedz:
+                return 1.5;
+            case Zelazo:
+                return 1;
+            case Cynk:
+                return 2.3;
+            case Olow:
+                return 2.5;
+            case Cyna:
+                return 2.8;
+            case Magnez:
+                return 2;
+            case Krzem:
+                return 1;
+            case Nikiel:
+                return 1;
+            default:
+                return -1;
 
         }
 
