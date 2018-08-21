fa149cfde9279184882cb186fc1f1f5c935fa98f
arturro96 <arturwolak22@gmail.com>
2018-08-21 11:56:30 +0200

    UIAgent edit

--- src/main/java/agh/agents/UIAgent.java
+++ src/main/java/agh/agents/UIAgent.java
@@ -75,90 +75,9 @@ public class UIAgent extends Agent implements InterfaceUI{
         return dbQueryFinished;
     }
 
-    //process run interface for GUI
-//    public double runProcess(Double[] metals, int mass, int meltingTemp, int metlingTime, int coolingTemp1, int heatingTime1, int coolingTemp2, int heatingTime2, int level){
-//        addBehaviour(new Behaviour() {
-//            @Override
-//            public void action() {
-//                //System.out.println("UIAgent----" + Arrays.toString(metals));
-//                String msgContent = Arrays.toString(metals)+" "+
-//                        Integer.toString(mass)+" "+
-//                        Integer.toString(meltingTemp)+" "+
-//                        Integer.toString(metlingTime)+" "+
-//                        Integer.toString(coolingTemp1)+" "+
-//                        Integer.toString(heatingTime1)+" "+
-//                        Integer.toString(coolingTemp2)+" "+
-//                        Integer.toString(heatingTime2)+" "+
-//                        Integer.toString(level);
-//                MessageTemplate msgTmp;
-//                ACLMessage msgReceive;
-//                switch(runProcessStep){
-//
-//                    case(0):
-//                        ACLMessage msgProcessInit = new ACLMessage(AgentMessages.START_PROCESS_AGENT);
-//                        msgProcessInit.setContent("");
-//                        msgProcessInit.addReceiver(new AID( args[0].toString(), AID.ISLOCALNAME));
-//                        send(msgProcessInit);
-//                        runProcessStep = 1;
-//                        block();
-//
-//                    case(1):
-//                        msgTmp = MessageTemplate.MatchPerformative(AgentMessages.START_PROCESS_AGENT_ACK);
-//                        msgReceive = receive(msgTmp);
-//                        if(msgReceive!=null){
-//                            runProcessStep = 2;
-//                        }
-//                        break;
-//
-//                    case(2):
-//                        ACLMessage msgSetValues = new ACLMessage(AgentMessages.SET_PROCESS_VALUES);
-//                        msgSetValues.setContent(msgContent);
-//                        msgSetValues.addReceiver(new AID( args[0].toString(), AID.ISLOCALNAME));
-//                        send(msgSetValues);
-//                        runProcessStep = 3;
-//                        break;
-//
-//                    case(3):
-//                        msgTmp = MessageTemplate.MatchPerformative(AgentMessages.SET_PROCESS_VALUES_ACK);
-//                        msgReceive = receive(msgTmp);
-//                        if(msgReceive!=null){
-//                            runProcessStep = 4;
-//                        }
-//                        break;
-//
-//                    case(4):
-//                        ACLMessage msgStartProcess = new ACLMessage(AgentMessages.START_PROCESS);
-//                        msgStartProcess.setContent("");
-//                        msgStartProcess.addReceiver(new AID( args[0].toString(), AID.ISLOCALNAME));
-//                        send(msgStartProcess);
-//                        runProcessStep = 5;
-//                        break;
-//
-//                    case(5):
-//                        msgTmp = MessageTemplate.MatchPerformative(AgentMessages.RECEIVE_RESULT);
-//                        msgReceive = receive(msgTmp);
-//                        if(msgReceive!=null){
-//                            runProcessFinished = true;
-//                            agentResult = Double.parseDouble(msgReceive.getContent());
-//                        }
-//                        break;
-//                    default:
-//                        break;
-//                }
-//            }
-//
-//            @Override
-//            public boolean done() {
-//                return runProcessFinished;
-//            }
-//        });
-//
-//        runProcessFinished = false;
-//        runProcessStep = 0;
-//        return agentResult;
-//    }
 
     public String runProcess(String[] data, int classifier) {
+        agentResult = null;
         addBehaviour(new Behaviour() {
             @Override
             public void action() {
